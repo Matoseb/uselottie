d51edeea15b49fd736bf175895a749ffdbc40462
Sébastien Matos <matos.sebastien@gmail.com>
2024-09-25 15:43:18 +0200

    add build

--- build.js
+++ build.js
@@ -2,21 +2,15 @@ import * as esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 import { nodeExternalsPlugin } from "esbuild-node-externals";
 import path from "path";
-import { fileURLToPath } from "url";
-
-import esbuildPluginTsc from "esbuild-plugin-tsc";
 import pkg from "npm-dts";
 
 const ENTRY = "./src/uselottie/index.ts";
 const OUT_DIR = "./build";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// new pkg.Generator({
-//   entry: ENTRY,
-//   output: path.join(OUT_DIR, "index.d.ts"),
-// }).generate();
+new pkg.Generator({
+  entry: ENTRY,
+  output: path.join(OUT_DIR, "index.d.ts"),
+}).generate();
 
 const sharedConfig = {
   entryPoints: [ENTRY],
@@ -39,9 +33,6 @@ await esbuild.build({
   plugins: [
     ...sharedConfig.plugins,
     nodeExternalsPlugin(),
-    esbuildPluginTsc({
-      tsconfig: path.join(__dirname, "tsconfig.json"),
-    }),
   ],
 });
 
---
+++ build/index.d.ts
@@ -0,0 +1,5 @@
+
+declare module '@matoseb/uselottie' {
+  import main = require('@matoseb/uselottie/src/uselottie/index');
+  export = main;
+}
