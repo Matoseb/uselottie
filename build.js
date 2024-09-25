667d3c3bd05ef66f3202b1c2cdabeeedea86bb9e
Sébastien Matos <matos.sebastien@gmail.com>
2024-09-25 15:36:16 +0200

    add types

--- build.js
+++ build.js
@@ -2,13 +2,28 @@ import * as esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 import { nodeExternalsPlugin } from "esbuild-node-externals";
 import path from "path";
+import { fileURLToPath } from "url";
+
+import esbuildPluginTsc from "esbuild-plugin-tsc";
+import pkg from "npm-dts";
+
+const ENTRY = "./src/uselottie/index.ts";
+const OUT_DIR = "./build";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// new pkg.Generator({
+//   entry: ENTRY,
+//   output: path.join(OUT_DIR, "index.d.ts"),
+// }).generate();
 
 const sharedConfig = {
-  entryPoints: ["./src/uselottie/index.ts"],
+  entryPoints: [ENTRY],
   bundle: true,
   minify: true,
   treeShaking: true,
-  outdir: "./build",
+  outdir: OUT_DIR,
   plugins: [
     sassPlugin({
       type: "css-text",
@@ -21,7 +36,13 @@ await esbuild.build({
   platform: "node",
   format: "cjs",
   target: "node14",
-  plugins: [...sharedConfig.plugins, nodeExternalsPlugin()],
+  plugins: [
+    ...sharedConfig.plugins,
+    nodeExternalsPlugin(),
+    esbuildPluginTsc({
+      tsconfig: path.join(__dirname, "tsconfig.json"),
+    }),
+  ],
 });
 
 await esbuild.build({
