bcf4dd07016d74634b468ca201d3130b042c175a
Sébastien Matos <matos.sebastien@gmail.com>
2024-09-27 19:19:13 +0200

    add scaling + burger

---
+++ examples/burger/index.js
@@ -0,0 +1,22 @@
+import useLottie from "@matoseb/uselottie";
+
+const controller = useLottie({
+  container: ".lottie",
+  path: "/animations/burger/data.json",
+  debug: true,
+});
+
+const { onLoad, seek, play, getElem, isPlaying } = controller;
+
+const open = "on-start, on-end";
+const close = "close-start, close-end";
+
+onLoad(() => {
+  getElem().onclick = () => {
+    if (isPlaying(open)) {
+      play(close);
+    } else {
+      play(open);
+    }
+  };
+});
