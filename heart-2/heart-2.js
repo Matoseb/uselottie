71e727bd5d2f6935e7967f341c381ebcc05ff149
Matoseb <matos.sebastien@gmail.com>
2024-09-29 14:24:01 +0000

    Deploying to examples from @ Matoseb/uselottie@e64b0581ad1bcd25bba3b957091b14c251097d05 🚀

---
+++ heart-2/heart-2.js
@@ -0,0 +1,34 @@
+import useLottie from 'https://unpkg.com/@matoseb/uselottie/build/bundle/index.js';
+
+const ACTION = {
+  holding: "start, hold",
+  release: "hold, release-end",
+  off: "off-start, off-end",
+};
+
+const controller = useLottie({
+  container: ".lottie",
+  name: "Heart 2",
+  path: "/animations/heart-explode/data.json",
+  debug: true,
+  rateVariation: 0.1,
+  volumeVariation: 0.1,
+});
+
+const { onLoad, onComplete, seek, play, isPlaying, getElem, player } =
+  controller;
+
+onLoad(() => {
+  getElem(".heart").onpointerdown = () => {
+    if (isPlaying(ACTION.release)) return;
+    play(ACTION.holding);
+  };
+
+  window.onpointerup = () => {
+    if (isPlaying(ACTION.holding)) {
+      play(ACTION.release);
+    } else if (isPlaying(ACTION.release)) {
+      play(ACTION.off);
+    }
+  };
+});
