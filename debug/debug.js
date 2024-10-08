7934178dcd3eca392800e576a830f1209a9dbef5
Matoseb <matos.sebastien@gmail.com>
2024-10-08 18:37:59 +0000

    Deploying to examples from @ Matoseb/uselottie@faafa308e4482193840e046a3ea1a5237a72ca13 🚀

---
+++ debug/debug.js
@@ -0,0 +1,30 @@
+import useLottie from 'https://unpkg.com/@matoseb/uselottie/build/bundle/index.js';
+
+const controller = useLottie({
+  container: ".lottie",
+  path: "/animations/debug/data.json",
+  debug: true,
+});
+
+// Destructure controller methods
+const {
+  onLoad,
+  play,
+  isPlaying,
+  getElem,
+  setAnimVariation,
+  random,
+  player,
+  loop,
+} = controller;
+
+// setup
+onLoad(() => {
+  play("mark-1, mark-2", { loop: true });
+
+  player.setSpeed(0.1);
+
+  window.onclick = () => {
+    loop(false);
+  };
+});
